497813b551091879b5cdcb1068ca77df57474646
WangQiming <1358632259@qq.com>
2024-07-28 18:18:12 +0800

    目录迁移

--- src/.vuepress/sidebar/project.ts
+++ src/.vuepress/sidebar/project.ts
@@ -2,6 +2,14 @@ import { arraySidebar, SidebarArrayOptions } from "vuepress-theme-hope";
 
 export const projectSidebar: SidebarArrayOptions = arraySidebar([
   {
+    text: "数据库",
+    icon: "sql",
+    collapsible: true,
+    link: "sql/",
+    prefix: "sql/",
+    children: "structure",
+  },
+  {
     text: "后端开发",
     icon: "java",
     collapsible: true,
