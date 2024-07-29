5d7a838a138d86e6efe910bded1f143e9a6daaca
wangqiming <1359632259@qq.com>
2024-07-29 14:39:47 +0800

    增加百度统计功能

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -12,4 +12,19 @@ export default defineUserConfig({
 
   // 和 PWA 一起启用
   // shouldPrefetch: false,
+  head: [
+    [ // 百度统计
+      'script',
+      {},
+      `
+      var _hmt = _hmt || [];
+      (function() {
+        var hm = document.createElement("script");
+        hm.src = "https://hm.baidu.com/hm.js?d329c480cfe7d4d2af07d72164336050";
+        var s = document.getElementsByTagName("script")[0]; 
+        s.parentNode.insertBefore(hm, s);
+      })();
+      `
+    ]
+  ],
 });
