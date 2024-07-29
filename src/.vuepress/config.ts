dcab330d1693a84349860e6fc56b5466202ef546
wangqiming <1359632259@qq.com>
2024-07-29 16:02:02 +0800

    更新百度统计

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -17,13 +17,13 @@ export default defineUserConfig({
       'script',
       {},
       `
-      var _hmt = _hmt || [];
-      (function() {
-        var hm = document.createElement("script");
-        hm.src = "https://hm.baidu.com/hm.js?d329c480cfe7d4d2af07d72164336050";
-        var s = document.getElementsByTagName("script")[0]; 
-        s.parentNode.insertBefore(hm, s);
-      })();
+        var _hmt = _hmt || [];
+        (function() {
+          var hm = document.createElement("script");
+          hm.src = "https://hm.baidu.com/hm.js?8faecec8d7cc152efd21efc848faa3fc";
+          var s = document.getElementsByTagName("script")[0]; 
+          s.parentNode.insertBefore(hm, s);
+        })();
       `
     ]
   ],
